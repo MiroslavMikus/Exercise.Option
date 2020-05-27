45efe443bc0d5f9109852b39233ad5b44a8607ab
Miroslav Mikus <Miroslav.Mikus@outlook.com>
2020-05-27 21:51:49 +0200

    Add project files.

---
+++ Exercise.Option.FakeLogic/BusinessLogicOption.cs
@@ -0,0 +1,27 @@
+﻿using Optional;
+using Optional.Linq;
+
+namespace Exercise.Option
+{
+    public class BusinessLogicOption
+    {
+        public Option<Reservation> RentVehicke(int userId, int vehicleId)
+        {
+            var user = RepositoryOption.GetUser(userId)
+                .SomeWhen(a => !RepositoryOption.GetReservationsByUser(userId).HasValue);
+
+            var car = RepositoryOption.GetCar(vehicleId)
+                .SomeWhen(a => !RepositoryOption.GetReservationsByVehicle(vehicleId).HasValue);
+
+            
+
+
+            return new Reservation()
+            {
+                Id = 1,
+                UserId = userId,
+                VehicleId = vehicleId
+            }.Some();
+        }
+    }
+}
